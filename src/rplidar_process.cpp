d6de65dde849d0edc583f7359b29f99577a8f0a1
Sushrith Arkal <sushrith.arkal@gmail.com>
2018-04-01 17:03:39 +0530

    Finished implementation of OA Node
    Preprocesses the raw data of RPLidar and cuts off a threshold distance
    to make processing of data for path planning (by auto_nav_path_planner)
    easier and simpler.
    
    Co-authored-by: Anush S Kumar <anushkumar27@gmail.com>
    Co-authored-by: Sushrith Arkal <sushrith.arkal@gmail.com>

--- src/rplidar_process.cpp
+++ src/rplidar_process.cpp
@@ -1,43 +1,67 @@
 /** Authors
  * Anush S Kumar (anushkumar27@gmail.com)
  * Sushrith Arkal (sushrith.arkal@gmail.com)
+ * 
+ * Preprocesses the RPLidar raw data and cuts off a radius of max_obstacle_range
+ * for distances.
 **/
-
-// Note: This is currently a dummy node publishing simulated RPLidar values for development
 
 #include <ros/ros.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <sensor_msgs/LaserScan.h>
 #include <vector>
+#include <algorithm>
+
+// "Obstacle map" message
+// Returned to client (published for use)
+std_msgs::Float32MultiArray ob_map;
+float max_range;
+void rplidar_sub_cb(const sensor_msgs::LaserScan::ConstPtr& msg) {
+	if(!msg->ranges.empty()) {
+		std::vector<float> data(msg->ranges.size());
+		std::copy(msg->ranges.begin(), msg->ranges.end(), data.begin());
+		ob_map.data = std::move(data);
+		std::transform(
+			ob_map.data.begin(), 
+			ob_map.data.end(), 
+			ob_map.data.begin(), 
+			[](float dist) -> float {
+				return (dist > 0 && dist <= max_range) ? dist : std::numeric_limits<float>::infinity();
+			}
+		);
+	}
+}
 
 int main(int argc, char **argv) {   
-    // Initialise ros node and advertise to roscore
-    ros::init(argc, argv, "rplidar_process");
+	// Initialise ros node and advertise to roscore
+	ros::init(argc, argv, "rplidar_process");
 
-    // NodeHandle is the main access point to communications with the ROS system.
-    ros::NodeHandle nh;
+	// NodeHandle is the main access point to communications with the ROS system.
+	ros::NodeHandle nh;
 
-    // PUBLISHER
-    // To publish the pre processed RPLidar value float32[]
-    ros::Publisher oa_pub = nh.advertise<std_msgs::Float32MultiArray>
-            ("oa/data", 10);
+	// Get params for max range considered for obstacles
+	nh.param<float>("max_obstacle_range", max_range, 5.0f);
 
-    // The setpoint publishing rate MUST be faster than 2Hz
-    ros::Rate rate(20.0);
+	// PUBLISHER
+	// To publish the pre processed RPLidar value float32[]
+	ros::Publisher oa_pub = nh.advertise<std_msgs::Float32MultiArray>
+			("oa/data", 10);
+	ros::Subscriber rplidar_sub = nh.subscribe<sensor_msgs::LaserScan>
+			("/laser/scan", 10, rplidar_sub_cb);
 
-    std_msgs::Float32MultiArray ob_map;
+	ros::Rate rate(20.0);
 
-    std::vector<float> dummy_data(360);
-    std::fill(dummy_data.begin(), dummy_data.end(), -1);
+	std::vector<float> initial_data(360);
+	std::fill(initial_data.begin(), initial_data.end(), -1);
 
-    ob_map.data = dummy_data;
+	ob_map.data = initial_data;
 
-    while(ros::ok()) {
-	// Publish the current pose
-	oa_pub.publish(ob_map);
+	while(ros::ok()) {
+		// Publish the current pose
+		oa_pub.publish(ob_map);
+		ros::spinOnce();
+		rate.sleep();
+	}
 
-        ros::spinOnce();
-        rate.sleep();
-    }
-
-    return 0;
+	return 0;
 }
